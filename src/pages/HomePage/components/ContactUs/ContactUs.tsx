31ea242bd4af541e789bdb9cefefbc39e80ae5c3
Poghos Barseghyan <poghosbarseghyan@Poghoss-MacBook-Air.local>
2025-06-20 13:11:12 +0400

    did contact us component

--- src/pages/HomePage/components/ContactUs/ContactUs.tsx
+++ src/pages/HomePage/components/ContactUs/ContactUs.tsx
@@ -1,5 +1,5 @@
-import { Container } from "react-bootstrap"
-import styles from "./ContactUs.module.scss"
+import { Button, Container, Form } from "react-bootstrap";
+import styles from "./ContactUs.module.scss";
 
 const ContactUs = () => {
     return (
@@ -8,10 +8,35 @@ const ContactUs = () => {
                 <h4 className={styles.question}>If You Have a Question Just</h4>
                 <h2 className={styles.title}>Contact Us</h2>
                 <Container className={styles.contactUsContent}>
+                    <div className={styles.contactUsText}>
+                        <h5 className={styles.subtitle}>
+                            Need more information or want to discuss your project? Contact us today and we'll get back to you as soon as possible.
+                        </h5>
+                        <p className={styles.mail}><span>H</span>arvestanoc@vega-project.org</p>
+                        <p className={styles.phone}><span>P</span>+ 374 98 425 239</p>
+                        <p className={styles.address}><span>A</span>Yerevan, Manandyan 22</p>
+                    </div>
+                    <div className={styles.contactUsForm}>
+                        <Form>
+                            <Form.Group controlId="formEmail" className={styles.formGroup}>
+                                <Form.Control type="email" placeholder="Your Email" required className={styles.control} />
+                                <Form.Label>Your Email</Form.Label>
+                            </Form.Group>
+                            <Form.Group controlId="formPhone" className={styles.formGroup}>
+                                <Form.Control type="tel" placeholder="Your Phone Number" required className={styles.control}/>
+                                <Form.Label>Your Phone Number</Form.Label>
+                            </Form.Group>
+                            <Form.Group controlId="formMessage" className={styles.formGroup}>
+                                <Form.Control as="textarea" rows={3} placeholder="Your Message" required className={`${styles.control} ${styles.textarea}`} />
+                                <Form.Label>Your Message</Form.Label>
+                            </Form.Group>
+                            <Button type="submit" variant="primary" className={styles.submitButton}>Send</Button>
+                        </Form>
+                    </div>
                 </Container>
             </Container>
         </section>
-    )
-}
+    );
+};
 
-export default ContactUs
+export default ContactUs;
